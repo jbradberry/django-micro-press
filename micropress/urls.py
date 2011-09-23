2c965c0a75be129f429e40ade34ef608f8ceea27
Jeff Bradberry <jeff.bradberry@gmail.com>
2011-09-23 00:05:47 -0400

    Fix url spec for article list by issue.

--- micropress/urls.py
+++ micropress/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('micropress.views',
     (r'^$', 'article_list'),
-    (r'^issue/(?P<issue>\d+)/$', 'issue_list'),
+    url(r'^issue/(?P<issue>\d+)/$', 'article_list', name='issue_list'),
     (r'^article/(?P<slug>[-\w]+)/$', 'article_detail'),
     #(r'^new/$', 'article_create'),
 )
