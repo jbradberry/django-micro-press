a2762274b0fc302abccbf02158e86b32a75e3c80
Jeff Bradberry <jeff.bradberry@gmail.com>
2011-09-24 12:13:22 -0400

    Added limit_articles decorator to views.py, with correct filtering on realms.

--- micropress/views.py
+++ micropress/views.py
@@ -1,28 +1,41 @@
+from django.contrib.contenttypes.models import ContentType
 from django.views.generic.list_detail import object_list, object_detail
+from django.conf import settings
+from functools import wraps
 import models
 
 
-def _limit_articles(realm_object_id=None, realm_slug=None,
-                    realm_slug_field='realm__slug', **kwargs):
-    queryset = models.Article.objects.all()
-    if realm_object_id:
-        queryset = queryset.filter(realm__pk=realm_object_id)
-    elif realm_slug and realm_slug_field:
-        queryset = queryset.filter(**{realm_slug_field: realm_slug})
-    else:
-        raise AttributeError(
-            "View must be called with either a realm_object_id or"
-            " a realm_slug/realm_slug_field.")
-    return queryset
+def limit_articles(f):
+    @wraps(f)
+    def func(request, realm_content_type=None, realm_object_id=None,
+             realm_slug=None, realm_slug_field='slug', *args, **kwargs):
+        if realm_content_type is None:
+            realm_content_type = settings.DEFAULT_REALM_TYPE
+        app_label, model = realm_content_type.split('.')
+        realm_model = ContentType.objects.get(app_label=app_label,
+                                              model=model.lower())
+
+        if realm_slug and realm_slug_field:
+            realm_object_id = realm_model.get_object_for_this_type(**
+                {realm_slug_field: realm_slug}).id
+        elif realm_object_id is None:
+            raise AttributeError(
+                "View must be called with either a realm_object_id or"
+                " a realm_slug/realm_slug_field.")
+
+        qs = models.Article.objects.filter(object_id=realm_object_id,
+                                           content_type=realm_model)
+        return f(request, *args, queryset=qs, **kwargs)
+    return func
 
 
-def article_list(request, issue=None, **kwargs):
-    qs = _limit_articles(**kwargs)
+@limit_articles
+def article_list(request, issue=None, queryset=None, **kwargs):
     if issue is not None:
-        qs = qs.filter(issue=int(issue))
-    return object_list(request, qs, **kwargs)
+        queryset = queryset.filter(issue=int(issue))
+    return object_list(request, queryset, **kwargs)
 
 
+@limit_articles
 def article_detail(request, **kwargs):
-    qs = _limit_articles(**kwargs)
-    return object_detail(request, qs, **kwargs)
+    return object_detail(request, **kwargs)
