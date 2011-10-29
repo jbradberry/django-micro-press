76a40dcbbd2ab63c606a8822c197f85d2dc80b1c
Jeff Bradberry <jeff.bradberry@gmail.com>
2011-10-28 23:24:08 -0400

    Added CreatePressForm.
    Other apps should bring this in and use it alongside their game creation forms.

--- micropress/forms.py
+++ micropress/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from micropress.models import Article, Section
+from micropress.models import Article, Section, Press
 
 
 class ArticleForm(forms.ModelForm):
@@ -8,3 +8,22 @@ class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = ('title', 'slug', 'byline', 'section', 'body', 'markup_type')
+
+
+class CreatePressForm(forms.ModelForm):
+    create = forms.BooleanField(initial=True, required=False)
+
+    class Meta:
+        model = Press
+        exclude = ('content_type', 'object_id', 'realm')
+
+    def full_clean(self):
+        if self.data.get('create', False):
+            super(CreatePressForm, self).full_clean()
+        else:
+            self.cleaned_data = {}
+            self._errors = forms.util.ErrorDict()
+
+    def save(self, *args, **kwargs):
+        if self.cleaned_data:
+            return super(CreatePressForm, self).save(*args, **kwargs)
