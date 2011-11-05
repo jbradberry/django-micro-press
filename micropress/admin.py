92bf14f418b09a7b402301dad0ed054a6ec42452
Jeff Bradberry <jeff.bradberry@gmail.com>
2011-11-05 12:10:25 -0400

    Override the content_type field in the admin.
    Now it shows the full "app_label.model" name.

--- micropress/admin.py
+++ micropress/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
+from django.forms import ModelChoiceField
+from django.contrib.contenttypes.models import ContentType
 from micropress.models import Press, Issue, Section, Article
+
+
+class GenericModelChoiceField(ModelChoiceField):
+    def label_from_instance(self, obj):
+        return u"{0}.{1}".format(obj.app_label, obj.model)
 
 
 class PressAdmin(admin.ModelAdmin):
     list_display = ('name', 'realm')
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'content_type':
+            kwargs['form_class'] = GenericModelChoiceField
+        return super(PressAdmin, self).formfield_for_foreignkey(
+            db_field, request, **kwargs)
 
 
 class IssueAdmin(admin.ModelAdmin):
