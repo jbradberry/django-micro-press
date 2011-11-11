96855ef5baee62f63887d942854c065ad6943f87
Jeff Bradberry <jeff.bradberry@gmail.com>
2011-11-10 23:10:45 -0500

    Validate that Article.slug and press are unique_together.

--- micropress/forms.py
+++ micropress/forms.py
@@ -9,6 +9,15 @@ class ArticleForm(forms.ModelForm):
         model = Article
         fields = ('title', 'slug', 'byline', 'section', 'body', 'markup_type')
 
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        slug = cleaned_data.get('slug', '')
+        press = self.instance.press
+        if Article.objects.filter(press=press, slug=slug).exists():
+            raise forms.ValidationError(
+                "Article with slug '{0}' already exists.".format(slug))
+        return cleaned_data
+
 
 class CreatePressForm(forms.ModelForm):
     class Meta:
