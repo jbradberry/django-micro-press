d6af07e56bcda37ece910345507292d919c687a8
Jeff Bradberry <jeff.bradberry@gmail.com>
2011-11-11 20:25:24 -0500

    Removed slug field from the article form, and autogenerate on the backend.

--- micropress/forms.py
+++ micropress/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.template.defaultfilters import slugify
 from micropress.models import Article, Section, Press
 
 
@@ -7,15 +8,25 @@ class ArticleForm(forms.ModelForm):
 
     class Meta:
         model = Article
-        fields = ('title', 'slug', 'byline', 'section', 'body', 'markup_type')
+        fields = ('title', 'byline', 'section', 'body', 'markup_type')
 
-    def clean_slug(self):
-        slug = self.cleaned_data.get('slug', '')
+    def clean(self):
+        cleaned_data = self.cleaned_data
         press = self.instance.press
-        if Article.objects.filter(press=press, slug=slug).exists():
-            raise forms.ValidationError(
-                "Article with slug '{0}' already exists.".format(slug))
-        return slug
+        title = cleaned_data.get('title', '')
+        max_length = Article._meta.get_field('slug').max_length
+
+        slug, num, end = slugify(title), 1, ''
+        if len(slug) > max_length:
+            slug = slug[:max_length]
+        while press.article_set.filter(slug=slug+end).exists():
+            num += 1
+            end = "-{0}".format(num)
+            if len(slug) + len(end) > max_length:
+                slug = slug[:max_length - len(end)]
+
+        self.instance.slug = slug + end
+        return cleaned_data
 
 
 class CreatePressForm(forms.ModelForm):
