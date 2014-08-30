11957fcbf0a1fce135822b30c0f35b3986a939d2
Jeff Bradberry <jeff.bradberry@gmail.com>
2014-08-30 16:16:26 -0400

    Added a template tag for getting the press object.

---
+++ micropress/templatetags/press_tags.py
@@ -0,0 +1,18 @@
+from django import template
+from django.contrib.contenttypes.models import ContentType
+
+from ..models import Press
+
+register = template.Library()
+
+
+@register.assignment_tag(takes_context=True)
+def get_press(context, realm):
+    ct = ContentType.objects.get_for_model(realm)
+    qs = Press.objects.filter(content_type=ct, object_id=realm.pk)
+
+    press = None
+    if qs:
+        press = qs.get()
+
+    return press
