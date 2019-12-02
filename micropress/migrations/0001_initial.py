a06d9d43df0531424789de58698e7f29ec8710e7
Jeff Bradberry <jeff.bradberry@gmail.com>
2019-12-01 21:19:23 -0500

    Bring in Django 1.7
    and fix the initial migration, which was breaking with 1.7.

--- micropress/migrations/0001_initial.py
+++ micropress/migrations/0001_initial.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import migrations, models
-import jsonfield.fields
+from django.db import models, migrations
 from django.conf import settings
+import jsonfield.fields
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contenttypes', '0002_remove_content_type_name'),
+        ('contenttypes', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Article',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
                 ('title', models.CharField(max_length=128)),
                 ('slug', models.SlugField(max_length=128)),
                 ('byline', models.CharField(max_length=128)),
@@ -25,7 +25,7 @@ class Migration(migrations.Migration):
                 ('modified', models.DateTimeField(auto_now=True)),
                 ('body', models.TextField()),
                 ('body_html', models.TextField()),
-                ('markup_type', models.CharField(default=b'restructuredtext', max_length=32, choices=[(b'textile', b'textile'), (b'restructuredtext', b'restructuredtext'), (b'markdown', b'markdown')])),
+                ('markup_type', models.CharField(default=b'restructuredtext', max_length=32, choices=[(b'textile', b'textile'), (b'markdown', b'markdown'), (b'restructuredtext', b'restructuredtext')])),
                 ('extra_data', jsonfield.fields.JSONField(default={})),
                 ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
@@ -33,11 +33,12 @@ class Migration(migrations.Migration):
                 'ordering': ('-created', 'title'),
                 'get_latest_by': 'created',
             },
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Press',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
                 ('name', models.CharField(max_length=128)),
                 ('closed', models.BooleanField(default=False)),
                 ('object_id', models.PositiveIntegerField()),
@@ -46,23 +47,29 @@ class Migration(migrations.Migration):
             options={
                 'verbose_name_plural': 'presses',
             },
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Section',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
                 ('name', models.CharField(max_length=32)),
             ],
+            options={
+            },
+            bases=(models.Model,),
         ),
         migrations.AddField(
             model_name='article',
             name='press',
             field=models.ForeignKey(to='micropress.Press'),
+            preserve_default=True,
         ),
         migrations.AddField(
             model_name='article',
             name='section',
             field=models.ForeignKey(to='micropress.Section'),
+            preserve_default=True,
         ),
         migrations.AlterUniqueTogether(
             name='article',
