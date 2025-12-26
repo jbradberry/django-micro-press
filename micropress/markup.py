095a01e52eb7035fc73168e9e4b63e3ac2a58238
Jeff Bradberry <jeff.bradberry@gmail.com>
2025-12-26 09:35:02 -0500

    Bring in nh3 to do html sanitization

--- micropress/markup.py
+++ micropress/markup.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 
 import markdown
+import nh3
 
 
 MARKUP_FILTER_OPTS = getattr(settings, 'MARKUP_FILTER_OPTS', {})
@@ -9,5 +10,5 @@ MARKUP_FILTER_OPTS = getattr(settings, 'MARKUP_FILTER_OPTS', {})
 def process(html):
     html = markdown.markdown(html)
     if html:
-        return html
+        return nh3.clean(html, **MARKUP_FILTER_OPTS)
     return ''
